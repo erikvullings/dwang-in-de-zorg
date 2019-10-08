bae6f10a943ea64522da0062fca1a4db761d36c1
Erik Vullings <erik.vullings@gmail.com>
2019-10-08 23:02:18 +0200

    Add 'rechtsvorm' and change address to visiting address

--- src/template/form.ts
+++ src/template/form.ts
@@ -124,29 +124,88 @@ export const countries = [
   },
 ];
 
+const rechtsvorm = [
+  { id: 1, label: 'Eenmanszaak' },
+  { id: 2, label: 'Eenmanszaak met meer dan één eigenaar' },
+  { id: 3, label: 'NV/BV in oprichting op A-formulier' },
+  // { id: 5, label: 'Rederij' },
+  { id: 7, label: 'Maatschap' },
+  { id: 11, label: 'Vennootschap onder firma' },
+  { id: 12, label: 'NV/BV in oprichting op B-formulier' },
+  { id: 21, label: 'Commanditaire vennootschap met één beherende vennoot' },
+  { id: 22, label: 'Commanditaire vennootschap met meer dan een beherende vennoot' },
+  { id: 23, label: 'NV/BV in oprichting op B-formulier' },
+  { id: 40, label: 'Rechtspersoon in oprichting' },
+  { id: 41, label: 'Besloten vennootschap met gewone structuur' },
+  { id: 42, label: 'Besloten vennootschap blijkens statuten structuurvennootschap' },
+  { id: 51, label: 'Naamloze vennootschap met gewone structuur' },
+  { id: 52, label: 'Naamloze vennootschap blijkens statuten structuurvennootschap' },
+  { id: 53, label: 'Naamloze vennootschap beleggingsmaatschappij met veranderlijk kapitaal' },
+  {
+    id: 54,
+    label:
+      'Naamloze vennootschap beleggingsmaatschappij met veranderlijk kapitaal blijkens statuten structuurvennootschap',
+  },
+  { id: 61, label: 'Coöperatie ua met gewone structuur' },
+  { id: 62, label: 'Coöperatie ua blijkens statuten structuurcoöperatie' },
+  { id: 63, label: 'Coöperatie wa met gewone structuur' },
+  { id: 64, label: 'Coöperatie wa blijkens statuten structuurcoöperatie' },
+  { id: 65, label: 'Coöperatie ba met gewone structuur' },
+  { id: 66, label: 'Coöperatie ba blijkens statuten structuurcoöperatie' },
+  { id: 71, label: 'Vereniging met volledige rechtbevoegdheid' },
+  { id: 72, label: 'Vereniging met beperkte rechtsbevoegdheid' },
+  // { id: 73, label: 'Kerkgenootschap' },
+  { id: 74, label: 'Stichting' },
+  { id: 81, label: 'Onderlinge waarborgmaatschappij ua met gewone structuur' },
+  { id: 82, label: 'Onderlinge waarborgmaatschappij ua blijkens statuten structuur-onderlinge' },
+  { id: 83, label: 'Onderlinge waarborgmaatschappij wa met gewone structuur' },
+  { id: 84, label: 'Onderlinge waarborgmaatschappij wa blijkens statuten structuur-onderlinge' },
+  { id: 85, label: 'Onderlinge waarborgmaatschappij ba met gewone structuur' },
+  { id: 86, label: 'Onderlinge waarborgmaatschappij ba blijkens statuten structuur-onderlinge' },
+  { id: 91, label: 'Buitenlandse rechtsvorm met hoofdvestiging in Nederland' },
+  { id: 92, label: 'Nevenvestiging met hoofdvestiging in buitenland' },
+  { id: 93, label: 'Europees economisch samenwerkingsverband (EESV)' },
+  { id: 94, label: 'Buitenlandse EG-vennootschap met onderneming in Nederland' },
+  { id: 95, label: 'Buitenlandse EG-vennootschap met hoofdnederzetting in Nederland' },
+  { id: 96, label: 'Buitenlandse op EG-vennootschap lijkende vennootschap met onderneming in Nederland' },
+  { id: 97, label: 'Buitenlandse op EG-vennootschap lijkende vennootschap met hoofdnederzetting in Nederland' },
+];
+
 const ActivityForm: Form = [
   { type: 'md', value: '###### Activiteit/aantekening' },
-  { id: 'datumIngang', label: 'Ingangsdatum', placeholder: 'Kies een datum', type: 'date', required: true, className: 'col s6' },
+  {
+    id: 'datumIngang',
+    label: 'Ingangsdatum',
+    placeholder: 'Kies een datum',
+    type: 'date',
+    required: true,
+    className: 'col s6',
+  },
   { id: 'datumEinde', label: 'Einddatum', placeholder: 'Kies een datum', type: 'date', className: 'col s6' },
 ];
 
 export const careOptions = [
-  { id: 'bejegening', label: 'Bejegening' },
-  { id: 'verzorging', label: 'Verzorging' },
-  { id: 'verpleging', label: 'Verpleging' },
-  { id: 'behandeling', label: 'Behandeling' },
-  { id: 'begeleiding', label: 'Begeleiding' },
-  { id: 'bescherming', label: 'Bescherming' },
-  { id: 'toediening', label: 'Toedienen van vocht, voeding en medicatie, verrichten van medische controles of handelingen, therapeutische maatregelen' },
+  {
+    id: 'toediening',
+    label:
+      'Toedienen van vocht, voeding en medicatie, verrichten van medische controles of handelingen, therapeutische maatregelen',
+  },
   { id: 'beperken_bewegingsvrijheid', label: 'Beperken van de bewegingsvrijheid' },
   { id: 'insluiten', label: 'Insluiten' },
   { id: 'toezicht', label: 'Uitoefenen van toezicht' },
   { id: 'onderzoek_kleding', label: 'Onderzoek aan kleding of lichaam' },
-  { id: 'onderzoek_woonruimte', label: 'Onderzoek van de woon- of verblijfsruimte op gedrag-beïnvloedende middelen en gevaarlijke voorwerpen' },
+  {
+    id: 'onderzoek_woonruimte',
+    label: 'Onderzoek van de woon- of verblijfsruimte op gedrag-beïnvloedende middelen en gevaarlijke voorwerpen',
+  },
   { id: 'drugs_controle', label: 'Controleren op aanwezigheid van gedrag-beïnvloedende middelen' },
   { id: 'beperken_vrijheid', label: 'Beperken in de vrijheid het eigen leven in te richten' },
   { id: 'beperken_bezoeksrecht', label: 'Beperken van het recht op het ontvangen van bezoek' },
-  { id: 'ontnemen_vrijheid', label: 'Ontnemen van de vrijheid van betrokkene door hem over te brengen naar een plaats die geschikt is voor tijdelijk verblijf' },
+  {
+    id: 'ontnemen_vrijheid',
+    label:
+      'Ontnemen van de vrijheid van betrokkene door hem over te brengen naar een plaats die geschikt is voor tijdelijk verblijf',
+  },
   { id: 'opnemen', label: 'Opnemen in een accommodatie' },
 ];
 
@@ -155,13 +214,24 @@ const CareForm: Form = [
   { type: 'md', value: '###### Welke type zorg wordt geleverd?' },
   { id: 'isAmbulantGeleverd', type: 'checkbox', className: 'col s12 m6', label: 'Is ambulant geleverd?' },
   { id: 'isKlinischGeleverd', type: 'checkbox', className: 'col s12 m6', label: 'Is klinisch geleverd?' },
-  { id: 'zorgvorm', type: 'options', options: careOptions, multiple: true, required: true, checkboxClass: 'col s12', show: ['isEenWvggzLocatie = true'] },
+  {
+    id: 'zorgvorm',
+    type: 'options',
+    options: careOptions,
+    multiple: true,
+    required: true,
+    checkboxClass: 'col s12',
+    show: ['isEenWvggzLocatie = true'],
+  },
   ...ActivityForm,
 ];
 
 const AddressForm: Form = [
-  { type: 'md', value: `&nbsp;
-###### Adresgegevens` },
+  {
+    type: 'md',
+    value: `&nbsp;
+###### Bezoekadres gegevens`,
+  },
   { id: 'postcode', label: 'Postcode', type: 'text', className: 'col s6 m4' },
   { id: 'huisnummer', label: 'Huisnummer', type: 'number', className: 'col s6 m2' },
   { id: 'huisletter', label: 'Huisletter', type: 'text', className: 'col s6 m2' },
@@ -198,7 +268,7 @@ export const CareProviderForm: Form = [
   },
   { id: 'naam', label: 'Zorgaanbieder', type: 'text', required: true, className: 'col s12 m8' },
   { id: 'kvk', label: 'KvK-nummer', type: 'number', required: true, className: 'col s12 m4' },
-  { id: 'rechtsvorm', required: true, type: 'textarea', className: 'col s12' },
+  { id: 'rechtsvorm', required: true, type: 'select', options: rechtsvorm, className: 'col s12' },
   ...AddressForm,
   { type: 'md', value: 'Voeg op de volgende pagina uw locaties toe (zie menu links).' },
 
