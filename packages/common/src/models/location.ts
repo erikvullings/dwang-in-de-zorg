1822196d0dfc8b672f6c7ccb2e38f56f841d93ce
Erik Vullings <erik.vullings@gmail.com>
2019-10-19 00:04:01 +0200

    Aantekeningen added to location

--- packages/common/src/models/location.ts
+++ packages/common/src/models/location.ts
@@ -2,17 +2,16 @@ import { IActivity } from './activity';
 import { IAddress } from './address';
 import { IQueryable } from './care-provider';
 
-export interface ILocation extends IAddress, IQueryable, IActivity {
+export interface ILocation extends IAddress, IQueryable {
   locatienaam: string;
   locatieomschrijving: string;
   vestigingsnummer?: number;
   /** Is de locatie een accommodatie (geschikt voor opname) */
-  isEenAccommodatie: boolean;
+  isAccommodatie: boolean;
   /** Wordt op de locatie Wzd-zorg geleverd */
-  isEenWzdLocatie?: boolean;
+  isWzd?: boolean;
   /** Wordt op de locatie Wvggz zorg geleverd */
-  isEenWvggzLocatie?: boolean;
-
-  /** RoWe: een locatie heeft 1..n "activity records" */
-  // activity: IActivity[];
+  isWvggz?: boolean;
+  /** Een locatie heeft 1..n "activity records" */
+  aantekeningen: IActivity[];
 }
