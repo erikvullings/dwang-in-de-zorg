d536972fc0be1bf28361085abffc343b39ba2ff3
Erik Vullings <erik.vullings@gmail.com>
2019-10-30 16:32:27 +0100

    If process.env.SERVER is set (e.g. during dev) use it, otherwise use window.location to access DB.

--- packages/app/src/models/app-state.ts
+++ packages/app/src/models/app-state.ts
@@ -1,5 +1,5 @@
 /** During development, use this URL to access the server. */
-const apiService = `http://localhost:${process.env.LOKI_PORT}`;
+const apiService = process.env.SERVER || window.location.origin; // `http://localhost:${process.env.LOKI_PORT}/`;
 
 /** Application state */
 export const AppState = {
