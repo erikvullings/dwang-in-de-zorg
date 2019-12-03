3cbc192f82d66ee6fc8d97c43b70dfc60aa2b801
Erik Vullings <erik.vullings@gmail.com>
2019-12-03 21:05:28 +0100

    FIX display error when aant is empty

--- packages/common/src/utils/index.ts
+++ packages/common/src/utils/index.ts
@@ -68,7 +68,7 @@ export const removeEmpty = <T>(obj: { [key: string]: any }): T => {
 };
 
 export const isLocationActive = (loc: ILocation) => {
-  if (!loc.aant || !(loc.isWvggz || loc.isWzd)) {
+  if (!loc.aant || loc.aant.length === 0 || !(loc.isWvggz || loc.isWzd)) {
     return false;
   }
   const { di: datumIngang, de: datumEinde } = loc.aant[loc.aant.length - 1];
